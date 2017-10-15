95beeb93f79a11bad4959290bb25c3945f1a984f
XetRAHF <zerqqr1@live.cn>
2017-10-15 15:00:16 +0800

    Third Spider Proble Fixed

--- INC500LIST/spiders/INC500_3.py
+++ INC500LIST/spiders/INC500_3.py
@@ -66,20 +66,29 @@ class INC500_3:
     def start_extraction(self):
         """Initialize output companies according to the result of spider 1"""
         for company in self.companies:
-
             cs = CompanySchema()
-            cs.company_schema['CompanyID'] = company['id']
-            cs.company_schema['Ranking'] = company['rank']
-            cs.company_schema['CompanyName'] = company['company']
-            cs.company_schema['Industry'] = company['industry']
-            cs.company_schema['Growth'] = company['growth']
-            cs.company_schema['Revenue'] = company['revenue']
-            cs.company_schema['City'] = company['city']
-            cs.company_schema['StateAbbr'] = company['state_s']
-            cs.company_schema['StateName'] = company['state_l']
-            cs.company_schema['YearsOnINCList'] = company['yrs_on_list']
-            for parterner in company['partner_lists']:
-                cs.company_schema['Partner'] += parterner + " "
+            if 'id' in company:
+                cs.company_schema['CompanyID'] = company['id']
+            if 'rank' in company:
+                cs.company_schema['Ranking'] = company['rank']
+            if 'company' in company:
+                cs.company_schema['CompanyName'] = company['company']
+            if 'industry' in company:
+                cs.company_schema['Industry'] = company['industry']
+            if 'growth' in company:
+                cs.company_schema['Growth'] = company['growth']
+            if 'revenue' in company:
+                cs.company_schema['Revenue'] = company['revenue']
+            if 'city' in company:
+                cs.company_schema['City'] = company['city']
+            if 'state_s' in company:
+                cs.company_schema['StateAbbr'] = company['state_s']
+            if 'state_l' in company:
+                cs.company_schema['StateName'] = company['state_l']
+            if 'yrs_on_list' in company:
+                cs.company_schema['YearsOnINCList'] = company['yrs_on_list']
+            for partner in company['partner_lists']:
+                cs.company_schema['Partner'] += partner + " "
 
             for wikipedia_detail in self.companies_info['wikipedias']:
                 if str(company['id']) == str(wikipedia_detail['company_id']):
@@ -89,25 +98,30 @@ class INC500_3:
             for company_profile in self.companies_info['inc_500_company_details']:
                 if str(company['id']) == str(company_profile['company_id']):
                     f = open(file=company_profile['file_path'], encoding='utf-8', mode='r')
-                    html = f.read().decode('utf-8')
-                    print(html)
+                    html = f.read()
                     f.close()
 
-                    cs.company_schema['BriefDescription'] = Selector(text=html).xpath(self.xpath_brief_description).extract()[0]
-                    cs.company_schema['Description'] = Selector(text=html).xpath(self.xpath_description).extract()[0]
-                    cs.company_schema['Leadership'] = Selector(text=html).xpath(self.xpath_leadership).extract()[0]
-                    cs.company_schema['Founded'] = Selector(text=html).xpath(self.xpath_founded).extract()[0]
-                    cs.company_schema['ThreeYearGrowth'] = Selector(text=html).xpath(self.xpath_3_year_growth).extract()[0]
-                    cs.company_schema['Employees'] = Selector(text=html).xpath(self.xpath_employees).extract()[0]
-                    cs.company_schema['Website'] = Selector(text=html).xpath(self.xpath_company_website).extract()[0]
-                    cs.company_schema['Location'] = Selector(text=html).xpath(self.xpath_location).extract()[0]
+                    if len(Selector(text=html).xpath(self.xpath_brief_description).extract()) > 0:
+                        cs.company_schema['BriefDescription'] = Selector(text=html).xpath(self.xpath_brief_description).extract()[0]
+                    if len(Selector(text=html).xpath(self.xpath_description).extract()) > 0:
+                        cs.company_schema['Description'] = Selector(text=html).xpath(self.xpath_description).extract()[0]
+                    if len(Selector(text=html).xpath(self.xpath_leadership).extract()) > 0:
+                        cs.company_schema['Leadership'] = Selector(text=html).xpath(self.xpath_leadership).extract()[0]
+                    if len(Selector(text=html).xpath(self.xpath_founded).extract()) > 0:
+                        cs.company_schema['Founded'] = Selector(text=html).xpath(self.xpath_founded).extract()[0]
+                    if len(Selector(text=html).xpath(self.xpath_3_year_growth).extract()) > 0:
+                        cs.company_schema['ThreeYearGrowth'] = Selector(text=html).xpath(self.xpath_3_year_growth).extract()[0]
+                    if len(Selector(text=html).xpath(self.xpath_employees).extract()) > 0:
+                        cs.company_schema['Employees'] = Selector(text=html).xpath(self.xpath_employees).extract()[0]
+                    if len(Selector(text=html).xpath(self.xpath_company_website).extract()) > 0:
+                        cs.company_schema['Website'] = Selector(text=html).xpath(self.xpath_company_website).extract()[0]
+                    if len(Selector(text=html).xpath(self.xpath_location).extract()) > 0:
+                        cs.company_schema['Location'] = Selector(text=html).xpath(self.xpath_location).extract()[0]
 
             output_companies.append(cs.company_schema)
 
             # For Debug purpose
             print(str(cs.company_schema['Ranking']) + "` " + "Finished information collecting stage")
-
-
 
         f = open(file='temp/INC500_3.json', encoding='utf-8', mode='w')
         f.write(json.dumps(output_companies))
